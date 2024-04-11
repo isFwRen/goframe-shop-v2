2698a9abaaec0810cf508d2063a78036890e6e98
Fengwangren <2629487604@qq.com>
2024-04-11 23:48:50 +0800

    add middleware and login

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"goframe-shop-v2/internal/controller"
+	"goframe-shop-v2/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -19,12 +20,18 @@ var (
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
 			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
+				//group.Middleware(ghttp.MiddlewareHandlerResponse)
+				group.Middleware(
+					//service.Middleware().CORS,
+					service.Middleware().Ctx,
+					service.Middleware().ResponseHandler,
+				)
 				group.Bind(
 					hello.NewV1(),
 					controller.Rotation, //轮播图
 					controller.Position, //手工位图
 					controller.Admin,    //手工位图
+					controller.Login,    //登录
 				)
 			})
 			s.Run()
